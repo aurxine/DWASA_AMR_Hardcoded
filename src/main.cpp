69aaa931bd46032a6e7050d29e83128aa4f46847
aurxine <sarowardhrubo@gmail.com>
2021-01-13 17:08:08 +0600

    individual characters are printed

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,15 @@
 
 String Gateway_Number = "01313791040";
 
-SoftwareSerial SIM800L(8, 9); // new (Rx, Tx) of pro mini
+SoftwareSerial SIM800L(9, 8); // new (Rx, Tx) of pro mini
+
+void Delete_Message(String location)
+{
+  SIM800L.println("AT+CMGD="+ location + "\r");
+}
 
 // For sending message
-void SendMessage(String message, String number)
+void Send_Message(String message, String number)
 {
   SIM800L.println("AT+CMGF=1");    
   delay(1000);
@@ -34,22 +39,22 @@ void SendMessage(String message, String number)
 }
 
 // For receiving message
-String readMessage()
+String Read_Message()
 {
-//   SIM800L.println("AT+CNMI=1,1,0,0,0"); 
-//   delay(1000);
+  // SIM800L.println("AT+CNMI=1,1,0,0,0"); 
+  // delay(1000);
 
-  SIM800L.println("AT+CMGL=\"REC UNREAD\"\r"); // To get the unread message
+  // SIM800L.println("AT+CMGL=\"REC UNREAD\"\r"); // To get the unread message
 
-  //SIM800L.write("AT+CMGL=\"ALL\"\r"); // For All stored messages
-  //Delete("8");
-  //SIM800L.write("AT+CMGR=1\r");
-  String data;
+  // SIM800L.write("AT+CMGL=\"ALL\"\r"); // For All stored messages
+  SIM800L.write("AT+CMGR=1\r");
+  String data ;//= "";
   if(SIM800L.available()>0)
   {
     data = SIM800L.readString();
-    Serial.println(data);
+    // Serial.println(data);
   }
+  Delete_Message("1");
 
   return data;
 }
@@ -64,19 +69,15 @@ String Message = "";
 
 void Break_Message(String msg)
 {
-    int sender_number_index = msg.indexOf("+880");
-    int time_stamp_index = sender_number_index + 22; //rough estimate
-    int message_index = time_stamp_index + 15; // rough estimate
+    int sender_number_index = 58;//msg.indexOf("+880");
+    int time_stamp_index = sender_number_index + 20; //rough estimate
+    int message_index = time_stamp_index + 20; // rough estimate
 
-    Message_Sender_Number = msg.substring(sender_number_index, 14);
-    Message_Time_Stamp = msg.substring(time_stamp_index, 14);
-    Message = msg.substring(message_index);
+    Message_Sender_Number = msg.substring(67, 80);
+    Message_Time_Stamp = msg.substring(87, 106);
+    Message = msg.substring(110);
 }
 
-void Delete(String location)
-{
-  SIM800L.println("AT+CMGD="+ location + "\r");
-}
 
 // this variable will count the meter pulses
 // when a certain amount of water passes, the meter will send a pulse through reed switch
@@ -123,10 +124,51 @@ void setup() {
   pinMode(Hall_Effect_Sensor_pin, INPUT_PULLUP);
     // pinMode(Reed_Switch_pin, INPUT_PULLUP);
   attachInterrupt(Hall_Effect_Sensor_pin, pulse_counter, FALLING);
+
+  //Send_Message("Hello", "01624593436");
+  // String msg = "";
+  // msg = Read_Message();
+  // Serial.println(msg);
+  // delay(3000);
+  // msg = Read_Message();
+  // Serial.println(msg);
+  // msg = Read_Message();
+  // Serial.println(msg);
   
 }
 
 void loop() {
+    String msg = "";
+    msg = Read_Message();
+    for(int i = 0; i< 10; i++)
+    {
+      Serial.print("#");
+    }
+    // Serial.println();
+    // Serial.println(msg);
+    // msg.replace("\n", "*");
+    for(int i = 0; i < msg.length(); i++)
+    {
+      Serial.print(i);
+      Serial.print(" :");
+      Serial.println(msg[i]);
+    }
+    //Serial.println(msg);
+
+    // Break_Message(msg);
+    // Serial.print("Number: ");
+    // Serial.print(Message_Sender_Number);
+    // Serial.print(" Time Stamp: ");
+    // Serial.print(Message_Time_Stamp);
+    // Serial.print(" Message: ");
+    // Serial.println(Message);
+    for(int i = 0; i< 10; i++)
+    {
+      Serial.print("#");
+    }
+    Serial.println();
+    
+    delay(3000);
 
 }
 
