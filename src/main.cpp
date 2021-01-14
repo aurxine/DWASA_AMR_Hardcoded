c285f91e38902439023e8f43cd4988fbd7c90e9e
aurxine <sarowardhrubo@gmail.com>
2021-01-14 10:08:08 +0600

    execute command and saving values implemented

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,14 @@
 // this pin will be used for hardware reset
 #define reset_pin 1
 
-String Gateway_Number = "01313791040";
+
+
+unsigned long int current_water_meter_reading = 0; // reading on the meter
+int meter_reading_save_address = 0; // starting address of EEPROM where meter reading will be saved
+int flow_per_pulse_save_address = sizeof(unsigned long int); // starting address of EEPROM where flow per pulse will be saved
+int flow_per_pulse; // water flow in liters per pulse
+
+String Gateway_Number = "01313791040"; // Number to which all communication will occur
 
 SoftwareSerial SIM800L(9, 8); // new (Rx, Tx) of pro mini
 
@@ -78,23 +85,48 @@ void Break_Message(String msg)
     Message = msg.substring(110);
 }
 
+void reset()
+{
+  Serial.println("Reseting");
+  current_water_meter_reading = 0;
+  counter = 0;
+  EEPROM.put(meter_reading_save_address, current_water_meter_reading);
+    // counter and total water will be reseted
+}
+
 bool Execute_Command(String Command)
 {
-  if(Command[0] == 'i')//Command looks like this init:xxxxxx
+  if(Command.indexOf("meter") > -1)//Command looks like this meter:xxxxxx
   {
-    String initial_water_meter_reading_str = Command.substring(5);
-    int initial_water_meter_reading = initial_water_meter_reading_str.toInt();
+    String initial_water_meter_reading_str = Command.substring(6);
+    current_water_meter_reading = initial_water_meter_reading_str.toInt();
+    EEPROM.put(meter_reading_save_address, current_water_meter_reading);
+    Serial.println(current_water_meter_reading);
+    return true;
+  }
+  else if(Command.indexOf("flow") > -1)//Command looks like this flow:xxxxxx
+  {
+    String flow_per_pulse_str = Command.substring(5);
+    flow_per_pulse = flow_per_pulse_str.toInt();
+    EEPROM.put(flow_per_pulse_save_address, flow_per_pulse);
+    Serial.println(flow_per_pulse);
     return true;
   }
   else if(Command.indexOf("getwater") > -1) // Command looks like ***getwater***
   {
     // send water meter reading
+    Send_Message(String(current_water_meter_reading), Gateway_Number);
+    return true;
   }
-  else if(Command[0] == 'r') // Command looks like reset
+  else if(Command.indexOf("reset") > -1) // Command looks like reset
   {
     reset();
+    return true;
   }
-  
+  else
+  {
+    return false;
+  }
 }
 // this variable will count the meter pulses
 // when a certain amount of water passes, the meter will send a pulse through reed switch
@@ -115,17 +147,14 @@ void pulse_counter()
     if(interrupt_time - last_interrupt_time > bounce_time)
     {
         counter++;
+        current_water_meter_reading += flow_per_pulse;
+        EEPROM.put(meter_reading_save_address, current_water_meter_reading); 
     }
 
     last_interrupt_time = interrupt_time;
   
 }
 
-
-void reset()
-{
-    // counter and total water will be reseted
-}
 
 
 void setup() {
@@ -141,6 +170,15 @@ void setup() {
   pinMode(Hall_Effect_Sensor_pin, INPUT_PULLUP);
     // pinMode(Reed_Switch_pin, INPUT_PULLUP);
   attachInterrupt(Hall_Effect_Sensor_pin, pulse_counter, FALLING);
+  // bool check = Execute_Command("meter:1233141");
+  // Serial.println(check);
+  // check = Execute_Command("flow:100");
+  // Serial.println(check);
+  EEPROM.get(meter_reading_save_address, current_water_meter_reading);
+  EEPROM.get(flow_per_pulse_save_address, flow_per_pulse);
+
+  Serial.println(current_water_meter_reading);
+  Serial.println(flow_per_pulse);
 
   //Send_Message("Hello", "01624593436");
   // String msg = "";
@@ -151,41 +189,42 @@ void setup() {
   // Serial.println(msg);
   // msg = Read_Message();
   // Serial.println(msg);
+
   
 }
 
 void loop() {
-    String msg = "";
-    msg = Read_Message();
-    for(int i = 0; i< 10; i++)
-    {
-      Serial.print("#");
-    }
-    // Serial.println();
-    // Serial.println(msg);
-    // msg.replace("\n", "*");
-    for(int i = 0; i < msg.length(); i++)
-    {
-      Serial.print(i);
-      Serial.print(" :");
-      Serial.println(msg[i]);
-    }
-    //Serial.println(msg);
+    // String msg = "";
+    // msg = Read_Message();
+    // for(int i = 0; i< 10; i++)
+    // {
+    //   Serial.print("#");
+    // }
+    // // Serial.println();
+    // // Serial.println(msg);
+    // // msg.replace("\n", "*");
+    // for(int i = 0; i < msg.length(); i++)
+    // {
+    //   Serial.print(i);
+    //   Serial.print(" :");
+    //   Serial.println(msg[i]);
+    // }
+    // //Serial.println(msg);
 
-    // Break_Message(msg);
-    // Serial.print("Number: ");
-    // Serial.print(Message_Sender_Number);
-    // Serial.print(" Time Stamp: ");
-    // Serial.print(Message_Time_Stamp);
-    // Serial.print(" Message: ");
-    // Serial.println(Message);
-    for(int i = 0; i< 10; i++)
-    {
-      Serial.print("#");
-    }
-    Serial.println();
+    // // Break_Message(msg);
+    // // Serial.print("Number: ");
+    // // Serial.print(Message_Sender_Number);
+    // // Serial.print(" Time Stamp: ");
+    // // Serial.print(Message_Time_Stamp);
+    // // Serial.print(" Message: ");
+    // // Serial.println(Message);
+    // for(int i = 0; i< 10; i++)
+    // {
+    //   Serial.print("#");
+    // }
+    // Serial.println();
     
-    delay(3000);
+    // delay(3000);
 
 }
 
